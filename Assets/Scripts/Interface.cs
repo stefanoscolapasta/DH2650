ac5fc9d1052fb543a13aab3c9cc67cf52fdc46b7
Darpos <michaelmoraless@hotmail.com>
2023-03-30 14:53:27 +0200

    Added smellyCloud ability

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -11,10 +11,15 @@ namespace StarterAssets
         public int health = 100;
         public bool[] abilities = new bool[10]; 
         private StarterAssetsInputs playerInput;
-        private float abilityColdown1 = 0.5f;
+        private float abilityColdown1 = 0f;
+        private float abilityColdown2 = 0f;
+        private bool smellyAbilityActivated = false;
         public GameObject leafKunaii;
-        public Transform shootingPoint;
+        public GameObject smellyCloud;
+        private GameObject smelly;
+        private Transform shootingPoint;
         private GameObject self;
+        Vector3 pos = new Vector3(0f, 0.8f, 0f);
         // Start is called before the first frame update
         void Start()
         {
@@ -27,44 +32,67 @@ namespace StarterAssets
         void Update()
         
         {
-            if(abilityColdown1 >= 0f){
+            //Stuff that happens every GameTic
+            if(smellyAbilityActivated == true){
+                smelly.transform.position = self.transform.position + pos;
+            }
+
+            if(abilityColdown1 > 0f){
                 abilityColdown1 -= Time.deltaTime;
+            }
+            if(abilityColdown2 > 0f){
+                abilityColdown2 -= Time.deltaTime;
+                if(abilityColdown2 <= 0){
+                    Destroy(smelly, 1);
+                    smellyAbilityActivated = false;
+                }
+                
             }
 
             if(playerInput.ability1 && abilities[0] && abilityColdown1 <= 0){
                 abilityColdown1 = 0.5f;
                 leafKunai();
             }
-            if(playerInput.ability2 && abilities[0]){
-                Debug.Log("knas plz2");
+            if(playerInput.ability2 && abilities[1] && abilityColdown2 <= 0){
+                abilityColdown2 = 6.5f;
+                AoeSmellyCloud();
+               
             }
             if(playerInput.ability3 && abilities[0]){
                 Debug.Log("knas plz3");
             }
         }
-
+        //pick-up
         void OnTriggerEnter(Collider other)
         {
-            
             if(other.name == "LeafKunaiAbility"){
                 abilities[0] = true;
                 Debug.Log("LeafKunaiPickedUp");
                 Destroy(other.gameObject);
             }
+            if(other.name == "SmellyCloudAbility"){
+                abilities[1] = true;
+                Debug.Log("SmellyCloudPickedUp");
+                Destroy(other.gameObject);
+            }
         }
 
         void leafKunai(){
-            Vector3 pos = new Vector3(0f, 0.8f, 0f);
             
             GameObject leaf = UnityEngine.Object.Instantiate(leafKunaii,shootingPoint.position + self.transform.forward*0.5f + pos, Quaternion.identity);
             Vector3 rototot = self.transform.forward;
             leaf.transform.Rotate(90f + rototot.x, 0f+ rototot.y, 0f+ rototot.z, Space.Self);
             leaf.GetComponent<Rigidbody>().AddForce(transform.forward*10);
-            Debug.Log(leaf.GetComponent<Rigidbody>());
-            Destroy(leaf, 3);
+            Destroy(leaf, 2);
+            //Debug.Log(leaf.GetComponent<Rigidbody>());
             Debug.Log("shooting");
         }
 
+        void AoeSmellyCloud(){
+            smelly  = UnityEngine.Object.Instantiate(smellyCloud,self.transform.position + pos, Quaternion.identity);
+            smellyAbilityActivated = true;
+            Debug.Log("smellyCloudActivated");
+        }
     }
 }
 
