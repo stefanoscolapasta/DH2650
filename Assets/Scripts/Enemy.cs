e7d3d48179a89576ad7617359561b72568af00b4
Darpos <michaelmoraless@hotmail.com>
2023-04-05 01:40:35 +0200

    Fixed healthbar on enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,22 @@
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     GameObject g;
     GameObject p;
     public int speed = 3;
-    public int maxHealth = 10;
+    public int health = 10;
     private int currentHealth;
-    [SerializeField] private TextMeshProUGUI HealthBarUI;
+    public Slider healthBar;
 
     void Start(){
-        currentHealth = maxHealth;
-        //HealthBarUI.text = currentHealth + "/" + maxHealth;
         g = gameObject;
         p = GameObject.FindWithTag("Player");
         Debug.Log(g.tag);
     }
 
     void FixedUpdate() {
-        putHealhBarOnEnemy();
+        healthBar.value = health;
         if(g.tag == "Enemy"){
             g.transform.LookAt(p.transform.position);
             g.transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -29,24 +27,12 @@ public class Enemy : MonoBehaviour
     {
         if(other.tag == "Kunai"){
             Debug.Log("Test2");
-            currentHealth -= 5;
-            HealthBarUI.text = currentHealth + "/" + maxHealth;
-            if(currentHealth <= 0){
+            health -= 5;
+            if(health <= 0){
                 Destroy(g);
             }
             Debug.Log("LeafKunaiPickedUp");
             Destroy(other.gameObject);
         }
     }
-
-    void putHealhBarOnEnemy(){
-        Vector3 pos = g.transform.position + new Vector3(0,10,0);
-        Vector3 rot = g.transform.rotation.ToEulerAngles();
-
-        
-        HealthBarUI.transform.position = pos;
-        HealthBarUI.transform.Rotate(new Vector3(0,rot.y,0));
-    }
-
-
 }
