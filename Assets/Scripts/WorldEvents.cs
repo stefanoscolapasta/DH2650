3ca42a12eb1da04461867f0046d1b10ec05f6477
Pontus Prüzelius <pontuspr@kth.se>
2023-04-04 23:21:53 +0200

    added wave mechanic

--- Assets/Scripts/WorldEvents.cs
+++ Assets/Scripts/WorldEvents.cs
@@ -4,26 +4,53 @@ public class WorldEvents : MonoBehaviour
 {
     GameObject g;
     public GameObject rat;
-    const int[] enemyLimits = {3,5,9};
-    int spawnedEnemies = 1;
+    int[] enemyLimits = {3,5,9,12,20};
+    int spawnedEnemies = 0;
     MeshRenderer render;
     int currentWave = 0;
+    bool waveSpawned = false;
 
     void Start(){
         g = gameObject;
         render = GetComponent<MeshRenderer>();
     }
 
-    void Update() {
-        int spawn = Random.Range(1,100);
-        if(spawn == 1 && spawnedEnemies < enemyLimits[currentWave]){
+    void FixedUpdate() {
+       // int spawn = Random.Range(1,100);
+        
+        if(waveSpawned == false){
+            for(int i = 0; i < enemyLimits[currentWave]; i++){
+                int x = Random.Range(1,25);
+                //float z = Random.Range(0,render.bounds.size.z);
+                int z = Random.Range(1,25);
+                GameObject enemy = Instantiate(rat,new Vector3(x,0,z),Quaternion.identity);
+                enemy.tag = "Enemy";
+            }
+            waveSpawned = true;
+        }
+       // var spawnedEnemies : GameObject[];
+       // spawnedEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+      /*  if(spawn == 1 && spawnedEnemies < enemyLimits[currentWave]){
             spawnedEnemies++;
             //float x = Random.Range(0,render.bounds.size.x);
             int x = Random.Range(1,25);
             //float z = Random.Range(0,render.bounds.size.z);
             int z = Random.Range(1,25);
             GameObject enemy = Instantiate(rat,new Vector3(x,0,z),Quaternion.identity);
+            enemy.tag = "Enemy";
+        }*/
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0){
+            Debug.Log("Wave " + currentWave + " done");
+            currentWave++;
+            for(int i = 0; i < enemyLimits[currentWave]; i++){
+                int x = Random.Range(1,25);
+                //float z = Random.Range(0,render.bounds.size.z);
+                int z = Random.Range(1,25);
+                GameObject enemy = Instantiate(rat,new Vector3(x,0,z),Quaternion.identity);
+                enemy.tag = "Enemy";
+            }
         }
+
     }
 
     void OnTriggerEnter(Collider other)
