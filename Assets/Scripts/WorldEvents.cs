066ac06c2297813a273fe77138c3ff4f3f00f2a9
Darpos <michaelmoraless@hotmail.com>
2023-05-09 02:22:03 +0200

    Made enviroment scale with progression

--- Assets/Scripts/WorldEvents.cs
+++ Assets/Scripts/WorldEvents.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using static System.Math;
+using System.Collections;
 public class WorldEvents : MonoBehaviour
 {
     GameObject g;
@@ -7,7 +7,7 @@ public class WorldEvents : MonoBehaviour
     public GameObject boss;
     //public int[,] enemyLimits = new int[3,3]{{1,2,2},{2,3,5},{3,5,7}};
     public int[,] enemyLimits = new int[3,3]{{1,1,1},{1,1,1},{1,1,1}}; // FOR TESTING
-    public int[] totalEnemies = {5,10,15};
+    public int[] totalEnemies = {3,3,3};
     int spawnedEnemies = 0;
     MeshRenderer render;
     public int currentWave = 0;
@@ -27,6 +27,12 @@ public class WorldEvents : MonoBehaviour
     int oldAssigned = 0;
     int slainOnIsland = 0;
     bool bossSpawned = false;
+    
+    GameObject[] IslandVeg = new GameObject[3]; //Change this to 4 if only 4 islands is used
+    GameObject[][] arrayOfVegChild = new GameObject[3][];
+    bool[][] activeVeg = new bool[3][];
+    int vegIndex = 0;
+    float[][] vegScaler = new float[3][];
 
     void Start(){
         Islands[0] = GameObject.Find("0");
@@ -39,6 +45,31 @@ public class WorldEvents : MonoBehaviour
         playableLevels[2] = GameObject.Find("Spawn2");
         playableLevels[3] = GameObject.Find("Spawn3");
         
+        //Prep for all of vegetation to spawn
+        //----------------------------------------------------
+
+        IslandVeg[0] = GameObject.Find("Island0veg");
+        IslandVeg[1] = GameObject.Find("Island1veg");
+        IslandVeg[2] = GameObject.Find("Island2veg");
+
+        int counter = 0;
+        foreach (var vegG in IslandVeg)
+        {
+            arrayOfVegChild[counter] = new GameObject[vegG.transform.childCount];
+            activeVeg[counter] = new bool[vegG.transform.childCount];
+            vegScaler[counter] = new float[vegG.transform.childCount];
+            for (int i = 0; i < vegG.transform.childCount; i++){
+                vegScaler[counter][i] = 0.000001f;
+                arrayOfVegChild[counter][i] = vegG.transform.GetChild(i).gameObject;
+                arrayOfVegChild[counter][i].transform.localScale = new Vector3(vegScaler[counter][i],vegScaler[counter][i],vegScaler[counter][i]);
+                arrayOfVegChild[counter][i].active = false;
+                activeVeg[counter][i] = false; //prolly unnecessary but wanna make sure it works as intended.
+            }
+            counter ++;
+        }
+
+        //------------------------------------------------
+
         player = GameObject.FindWithTag("Player");
         //playableLevels = GameObject.FindGameObjectsWithTag("PlayableLevel");
         progressGrass = GameObject.FindGameObjectsWithTag("ProgressGress")[0];
@@ -62,7 +93,27 @@ public class WorldEvents : MonoBehaviour
     }
 
     void FixedUpdate() {
-        
+        int outer = 0;
+        int inner = 0;
+        foreach (var vegitationInProcessPerIsland in activeVeg)
+        {
+            foreach (bool b in vegitationInProcessPerIsland)
+            {
+                if(b){
+                    if(vegScaler[outer][inner] >= 1f){
+                        activeVeg[outer][inner] = false;
+                    }
+                    else{
+                        vegScaler[outer][inner] += 0.01f;
+                        arrayOfVegChild[outer][inner].transform.localScale = new Vector3(vegScaler[outer][inner],vegScaler[outer][inner],vegScaler[outer][inner]);
+                    }
+                }
+                inner ++;
+            }
+            outer ++;
+            inner = 0;
+        }
+
         if(waveSpawned == false && bossSpawned == false){
             Debug.Log("isla " + AssignedIsland);
             Debug.Log("wave " + currentWave);
@@ -82,19 +133,34 @@ public class WorldEvents : MonoBehaviour
             oldAssigned = AssignedIsland;
             player.transform.position = playableLevels[AssignedIsland].transform.position;
             slainOnIsland = 0;
+            vegIndex = 0;
         }
 
         if(slainEnemies > oldSlainEnemies){
             oldSlainEnemies ++;
-            //int totOnIsland = 0;
-           // foreach (int enemiesSpawn in enemyLimits){
-           //     totOnIsland += enemiesSpawn;
-            //}
+            int totOnIsland = 0;
+            foreach (int enemiesSpawn in enemyLimits){
+                totOnIsland += enemiesSpawn;
+            }
             slainOnIsland ++;
             Debug.Log(slainOnIsland);
+            float ratioOfKilledEnemies = (float) slainOnIsland/ (float) totalEnemies[AssignedIsland];
             float radiuss = islandWGD[AssignedIsland].x / 2;
-            float x = (((((float) slainOnIsland/ (float) totalEnemies[AssignedIsland]) * radiuss) + 1) *1.9f);
+            float x = (((ratioOfKilledEnemies * radiuss) + 1) *1.9f);
             progressGrassArray[AssignedIsland].gameObject.transform.localScale = new Vector3(x,0.00001f,x);
+            //Set veg on island to start animate()
+            float upperLimitOfVeg = (float) arrayOfVegChild[AssignedIsland].Length * (float) ratioOfKilledEnemies;
+            for(int i = vegIndex ; i <= upperLimitOfVeg; i ++){
+                if(i >= activeVeg[AssignedIsland].Length){
+                    break;
+                }
+                Debug.Log("vegIndex " + i);
+                Debug.Log("ratioOfKilledEnemies " + ratioOfKilledEnemies);
+                activeVeg[AssignedIsland][i] = true;
+                arrayOfVegChild[AssignedIsland][i].active = true; //not here
+                vegIndex = i;
+            }
+            vegIndex = (int) upperLimitOfVeg;
         }
             
         if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0){
